88dd78d09265b397e38c5e235de36c1985853ec2
Victor Trajano <trajanodev@hotmail.com>
2021-11-11 10:11:57 -0400

    Task finalizada

---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,80 @@
+import { useState } from 'react';
+import './Form.modules.css';
+
+export const Form = () => {
+
+    const [nome, setNome] = useState('')
+    const [email, setEmail] = useState('')
+    const [profissao, setProfissao] = useState('')
+    const [listaDeTrabalhadores, setListaDeTrabalhadores] = useState([])
+
+  
+
+    const cadastraTrabalhador = (e) => {
+        console.log(listaDeTrabalhadores)
+        e.preventDefault();
+        
+
+        if (nome === '' || email === '' || profissao === '') {
+            alert('Por favor não deixe nenhum campo vazio no cadastro.')
+            return false
+        }
+
+        setListaDeTrabalhadores([...listaDeTrabalhadores, {
+            id: listaDeTrabalhadores.length + 1,
+            nome: nome,
+            email: email,
+            profissao: profissao
+        }])
+    }
+
+    const removeUsuario = (id) => {
+        const trabalhadorRemovido = listaDeTrabalhadores.filter(t => t.id !== id)
+        setListaDeTrabalhadores(trabalhadorRemovido)
+    }
+
+    return (
+        <>
+            <div className="container">
+                <h2>Cadastro de trabalhadores</h2>
+                <form onSubmit={cadastraTrabalhador}>
+                    <div>
+                        <input
+                            type="text"
+                            placeholder="Digite o nome completo"
+                            onChange={(e) => setNome(e.target.value)}
+                        />
+                    </div>
+                    <div>
+                        <input
+                            type="text"
+                            placeholder="Digite o email"
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                    </div>
+                    <div>
+                        <input
+                            type="text"
+                            placeholder="Digite sua profissão"
+                            onChange={(e) => setProfissao(e.target.value)}
+                        />
+                    </div>
+                    <button type="submit">Cadastrar trabalhador</button>
+                </form>
+
+                {listaDeTrabalhadores.length > 0 && (<h2>Trabalhadores cadastrados</h2>)}
+
+                <div className="flex">
+                    {listaDeTrabalhadores.map(t => (
+                        <div className=" card">
+                            <h3>Usuário: {t.nome}</h3>
+                            <p>E-mail: {t.email}</p>
+                            <p>Profissão: {t.profissao}</p>
+                            <button onClick={() => removeUsuario(t.id)}>Remover</button>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </>
+    );
+}
